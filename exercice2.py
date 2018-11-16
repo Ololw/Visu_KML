9c12d9164e0ed1e8461ef2ce04bf8ab47ecbe111
OhRaph <44495363+OhRaph@users.noreply.github.com>
2018-11-16 15:35:58 +0100

    Add files via upload

--- exercice2.py
+++ exercice2.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
 import random
-latitude=random.randrange(-90,90)
-longitude=random.randrange(-180,180)
-kml=(
-'<?xmlversion ="1.0"encoding="UTF-8"?>\n'
-'<kml xmlns="http://www.opengis.net/kml/2.2">\n'
-#'<Placemark>\n'
-#'<name>Random Placemark</name>\n'
-#'<Point>\n'
-#'<coordinates>\n'
-'')
-#ajout des coordonnees aleatoires
-#kml+=str(longitude)+","+str(latitude)
-kml+=(
-#'</coordinates>\n'
-#'</Point>\n'
-#'</Placemark>\n'
-
-'<GroundOverlay>\n'
-'<name>Exemple d\' Overlay</name>\n'
-'<Icon>\n' 
-'<href>PlanCampus.jpg</href>\n'
-'</Icon>\n'
-'<LatLonBox>\n'
-'<north>45.1988667</north>\n'
-'<south>45.1848</south>\n'
-'<east>5.78475</east>\n'
-'<west>5.745333333333333</west>\n'
-'</LatLonBox>\n'
-'<color>ffffffff</color>\n'
-'</GroundOverlay>\n'
-
-'</kml>')
-#affichage sur la sortie standard
+kml = (
+  '<?xml version ="1.0" encoding="UTF-8"?>\n'
+  '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
+  )
+kml += (
+  '\t<GroundOverlay>\n'
+  '\t\t<name>Exemple d\'Overlay</name>\n'
+  '\t\t<Icon>\n'
+  '\t\t<href>data-tp2/PlanCampus.jpg</href>\n'
+  '\t\t</Icon>\n'
+  '\t\t<LatLonBox>\n'
+  '\t\t\t<north>45.2021</north>\n'
+  '\t\t\t<south>45.18377</south>\n'
+  '\t\t\t<east>5.78775</east>\n'
+  '\t\t\t<west >5.7450</west>\n'
+  '\t\t\t<rotation>-0.1556640799496235</rotation>\n'
+  '\t\t</LatLonBox>\n'
+  '\t\t<color>a0ffffff</color>\n'
+  '\t</GroundOverlay>\n'
+  '</kml>'
+  )
+# affichage sur la sortie standard
 print(kml)
-
